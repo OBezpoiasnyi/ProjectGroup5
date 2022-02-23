8564d2a04dcf7a4e8e004883d4581a5efdd0ec75
O_Bezpoiasnyi <obezpoiasnyi@gmail.com>
2022-02-23 11:54:12 -0600

    add terget to gitignore

--- src/test/java/com/nextbasecrm/utilities/WebDriverFactory.java
+++ src/test/java/com/nextbasecrm/utilities/WebDriverFactory.java
@@ -1,24 +1,13 @@
 package com.nextbasecrm.utilities;
 
-public class WebDriverFactory {
-
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class WebDriverFactory {
 
 
-
-
-        public static void main(String[] args) {
-
-            getDriver("firefox");
-
-
-
-        }
 
         public static WebDriver getDriver(String browserType ) {
 
@@ -34,9 +23,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
                 System.out.println("Given browser type does not exist");
                 return null;
             }
-
-
-
 
         }
 
