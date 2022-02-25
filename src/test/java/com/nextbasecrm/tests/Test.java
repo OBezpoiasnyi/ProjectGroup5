bcbf02ca5acdef80347e76f109916f61cc80275b
bledibledi <bledi@cybertekschool.com>
2022-02-25 18:47:14 +0100

    i added my test US1

--- src/test/java/com/nextbasecrm/tests/Test.java
+++ src/test/java/com/nextbasecrm/tests/Test.java
@@ -1,4 +1,7 @@
 package com.nextbasecrm.tests;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("TEst");
+    }
 }
---
+++ src/test/java/com/nextbasecrm/tests/US1LoginF.java
@@ -0,0 +1,8 @@
+package com.nextbasecrm.tests;
+
+public class US1LoginF {
+    public static void main(String[] args) {
+        System.out.println("This is my test1");
+        //Finished my test case
+    }
+}
