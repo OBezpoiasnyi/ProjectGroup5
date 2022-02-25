d116f4d5082009e4a13d3b247b5d3cec2fca3ae2
Cameron Cisneros <cisneroscameron16@gmail.com>
2022-02-25 12:21:46 -0600

    new commit

--- src/test/java/com/nextbasecrm/tests/Test.java
+++ src/test/java/com/nextbasecrm/tests/Test.java
@@ -2,6 +2,6 @@ package com.nextbasecrm.tests;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("TEst");
+        System.out.println("Cameron");
     }
 }
