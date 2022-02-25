4e3aa04c9a750734ba7aec5edc9be16aece306f0
sayad <sayadeligne@gmail.com>
2022-02-25 12:05:15 -0600

    I added test1

---
+++ src/test/java/com/nextbasecrm/tests/sayad/US10_CreateTaskTab.java
@@ -0,0 +1,9 @@
+package com.nextbasecrm.tests.sayad;
+
+public class US10_CreateTaskTab {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first test1");
+    }
+
+}
