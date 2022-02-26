6b44377f80a9c056e1bc622fdeee8c85af3cf1b5
Gulustan <gbakisli@gmail.com>
2022-02-26 10:42:24 -0600

    US7 is done

--- src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
+++ src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -33,8 +34,15 @@ public class US7_Gulustan {
         login.click();
 
         Assert.assertEquals(driver.findElement(By.xpath("//span[@class='menu-item-link-text']")).getText(), "Activity Stream");
-
     }
+   @AfterMethod
+           public void  tearDown(){
+
+        driver.close();
+   }
+
+
+
 
         @Test
                 public void test1() throws InterruptedException {
