3df5199f60fbce6ab848e8cd536f45dd3b9d7298
Gulustan <gbakisli@gmail.com>
2022-02-25 19:31:28 -0600

    US7_Gulustan

--- src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
+++ src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
@@ -1,10 +1,25 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.ConfigurationReader;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 public class US7_Gulustan {
 
+    public WebDriver driver;
 
+    @BeforeMethod
+    public void setUpMethod(){
 
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("env"));
 
+    }
 
 
 }
