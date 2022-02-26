6584351fa54837aad83b92fdc49f42643a905f0f
Gulustan <gbakisli@gmail.com>
2022-02-26 15:26:15 -0600

    US7 is done

--- src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
+++ src/test/java/com/nextbasecrm/tests/US7_Gulustan.java
@@ -1,9 +1,6 @@
 package com.nextbasecrm.tests;
 
-import com.nextbasecrm.utilities.BrowserUtils;
-import com.nextbasecrm.utilities.ConfigurationReader;
-import com.nextbasecrm.utilities.Username_Password;
-import com.nextbasecrm.utilities.WebDriverFactory;
+import com.nextbasecrm.utilities.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,45 +23,54 @@ public class US7_Gulustan {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(ConfigurationReader.getProperty("env"));
 
-        Username_Password.username1(driver);
 
-        WebElement rememberME_button = driver.findElement(By.id("USER_REMEMBER"));
-        rememberME_button.click();
-
-        WebElement login = driver.findElement(By.xpath("//input[@class='login-btn']"));
-        login.click();
-
-        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='menu-item-link-text']")).getText(), "Activity Stream");
     }
-   @AfterMethod
-           public void  tearDown(){
-        BrowserUtils.sleep(3);
-        driver.close();
-   }
 
 
 
+    @Test
+      public void crm_login_test(){
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("hr14@cybertekschool.com");
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+}
 
         @Test
                 public void test1()  {
-
+            CRM_Utilities.crm_login(driver);
 
             WebElement pollButton = driver.findElement(By.xpath("//span[.='Poll']/span"));
-           BrowserUtils.sleep(3);
+            BrowserUtils.sleep(3);
+
             pollButton.click();
+            BrowserUtils.sleep(3);
+
 
             WebElement clickOneAnswer=driver.findElement(By.xpath("(//span[@class='bx-vote-block-inp-substitute'])[1]"));
-           BrowserUtils.sleep(3);
+            BrowserUtils.sleep(3);
+
             clickOneAnswer.click();
+            BrowserUtils.sleep(3);
 
             WebElement clickVote=driver.findElement(By.xpath("(//button[@class='ui-btn ui-btn-lg ui-btn-primary'])[2]"));
-           BrowserUtils.sleep(3);
-            clickVote.click();
+            BrowserUtils.sleep(3);
 
+            clickVote.click();
+            BrowserUtils.sleep(3);
         }
 
 
-
+    @AfterMethod
+    public void  tearDown(){
+        BrowserUtils.sleep(3);
+        driver.close();
+    }
 
 
 
